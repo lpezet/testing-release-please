421884f9bd94c95b557e315aaf4bbd5a63ab3367
Luke <lpezet@gmail.com>
2024-07-20 11:36:26 +0200

    chore: [TIC-003] doing more (#10)
    * feat(projectA): testing scope.
    
    * Changed release-please workflow to work off main branch.

--- projectA/src/app.service.ts
+++ projectA/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World Changed Again!';
+    return 'Hello World Changed Again And Again!';
   }
 }
