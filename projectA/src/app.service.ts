936cf66fceb77db0aa7434135b688655cbbfd950
lpezet <lpezet@gmail.com>
2024-07-20 11:38:10 +0200

    [TIC-004] changing projectA only.

--- projectA/src/app.service.ts
+++ projectA/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World Changed Again And Again!';
+    return 'Hello World Changed Again And Again And Guess What...!';
   }
 }
