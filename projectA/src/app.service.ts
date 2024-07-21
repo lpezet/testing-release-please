1c4208d99eba3e41ac733a8e9908459dc719296b
lpezet <lpezet@gmail.com>
2024-07-21 18:04:57 +0200

    feat: added 456 to hello service in projectA.

--- projectA/src/app.service.ts
+++ projectA/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World Good 123!';
+    return 'Hello World Good 123 456!';
   }
 }
